633cdc9894b7c74a8f8a03e24d9c6eaa357ca285
Segamo <sgarcesmo@gmail.com>
2015-07-08 15:26:46 -0500

    Comienzo del proyecto con la clase Suma

---
+++ src/operacion/Suma.scala
@@ -0,0 +1,12 @@
+package operacion
+
+/**
+ * @author sebastian.garces
+ */
+object Suma {
+  def main(args: Array[String]): Unit = {
+    var a : Int = 1
+    var b : Int = 68
+    println(a + b)
+  }
+}
